896ddf2f938f1c0194c5c9c64ea67a8e862951a5
LAB5.1 <lab51@fatec.com>
2024-10-02 21:12:35 -0300

    Tentativa em aula

--- Assets/Scripts/Spaceship/MultiShotController.cs
+++ Assets/Scripts/Spaceship/MultiShotController.cs
@@ -1,15 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
-{
-    public static Projectile instance;
 
+public class MultiShotController : MonoBehaviour 
+{
     public float speed = 25f;
 
-    void Update()
+    private void Update()
     {
         transform.Translate(Time.deltaTime * speed * Vector3.up);
-        Instantiate(Projectile.instance, new Vector3(0, 5, 0), Quaternion.identity);
+        
     }
 
     void OnBecameInvisible()
